ea5f0ac2632c4135197fb42727fd86efd4b9c3d7
aquenneville <alainquenneville@gmail.com>
2018-03-12 22:41:59 +0000

    update removal of folders

--- remove-download-duplicate-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# cd in your Download directory
-cd ~/Downloads
-ls | egrep '.*\s\(\d\).pdf' | tr "\n" "\0" | xargs -0 rm -v
--- syno-external-ip/syno_external_ip.sh
+++
@@ -1,5 +0,0 @@
-#!#/bin/bash
-EMAIL=$1
-PORT=$2
-IP=$(wget http://ipinfo.io/ip -qO -)
-echo "external ip: https://${IP}:${PORT}" | /opt/bin/nail -s "Synology external ip" ${EMAIL}
