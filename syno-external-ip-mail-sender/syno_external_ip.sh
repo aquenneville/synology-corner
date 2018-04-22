96148aaa2e6d7f80692a532073fd68542c7a42f7
aquenneville <alainquenneville@gmail.com>
2018-04-22 23:01:03 +0100

    Update code after shellcheck linter

--- syno-external-ip-mail-sender/syno_external_ip.sh
+++ syno-external-ip-mail-sender/syno_external_ip.sh
@@ -2,4 +2,4 @@
 EMAIL=$1
 PORT=$2
 IP=$(wget http://ipinfo.io/ip -qO -)
-echo "external ip: https://${IP}:${PORT}" | /opt/bin/nail -s "Synology external ip" ${EMAIL}
+echo "external ip: https://${IP}:${PORT}" | /opt/bin/nail -s "Synology external ip ${EMAIL}"
